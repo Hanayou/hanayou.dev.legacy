991b64cf02c76099a89e1d5b6fd0badfcd42ed0d
Samuel Short <samuel.short@gmail.com>
2024-07-27 21:26:43 +0900

    Set lato font and primary colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#FFD700",
+        bgPrimary: "#22272E",
+        bgSecondary: "#181C22",
+        success: "#3BCD52",
+        error: "#E53E3E",
       },
     },
   },
