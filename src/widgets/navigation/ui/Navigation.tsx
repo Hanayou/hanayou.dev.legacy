fdcc89f435e43bc0f8355b3d431ff80ba5c40db2
Samuel Short <samuel.short@gmail.com>
2024-08-10 21:50:38 +0900

    Add Head shot

--- src/widgets/navigation/ui/Navigation.tsx
+++ src/widgets/navigation/ui/Navigation.tsx
@@ -4,7 +4,7 @@ import { NavigationItem } from "@/widgets/navigation/ui/NavigationItem";
 export const Navigation = (props: TNavigation) => {
   return (
     <nav>
-      <ul className="w-fit m-auto px-8 py-3 flex gap-x-[3.125rem]">
+      <ul className="fixed top-[3.125rem] left-[50%] translate-x-[-50%] w-fit m-auto px-8 py-3 flex gap-x-[3.125rem] rounded-full bg-bgPrimary/80 backdrop-blur-2xl">
         {props.items.map((item) => (
           <NavigationItem key={item.path} {...item} />
         ))}
