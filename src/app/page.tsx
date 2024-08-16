217b383fbc0d0d08bd2866851bf2949fa46cd693
Samuel Short <samuel.short@gmail.com>
2024-08-16 13:32:52 +0900

    Update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 import PrimaryButton from '@/widgets/button/ui/Button';
-import { Mail } from 'lucide-react';
+import SocialLinks from '@/widgets/social-links/ui/SocialLinks';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <main>
+        {/* Hero Section */}
         <div className="grid grid-cols-2 h-[1080px] items-center">
           <div className="flex flex-col gap-y-[2.6875rem] py-[1.5625rem] px-[2.375rem]">
             <h3>Hi, I'm <span className="text-primary">Sam</span></h3>
             <h1>Software<br/><span className="text-primary">Engineer /&gt;</span></h1>
             <p className="max-w-[30.75rem]">Passionate engineer with over 5 years of professional development and consulting experience. Always looking to expand knowledge and develop exciting, cutting edge products.</p>
             <PrimaryButton text="See My Projects" size="default" hasIcon={false} icon={<div />} />
-            <div>Social Links Component</div>
+            <SocialLinks />
           </div>
           
+          {/* Country Background Image */}
           <div className='m-auto z-0 relative w-[21.875rem] h-[21.875rem] bg-bgPrimary rounded-full'>
             <div className='absolute -z-10 pointer-events-none top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[114.6875rem] h-[98.9375rem] bg-[radial-gradient(40.00%_40.00%_at_50.00%_50.00%,rgba(113,202,252,0.30)_0%,rgba(58,66,79,0.00)_100%)] opacity-[72%]'></div>
             <div className='absolute z-20 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[20rem] h-[20rem] rounded-full bg-primary'>
@@ -27,9 +29,6 @@ export default function Home() {
           <h2>My <span className="text-primary">Skills</span></h2>
           <p>GRID</p>
           <h4 className="font-bold uppercase tracking-[0.5em]">languages</h4>
-          <PrimaryButton text="See My Projects" size="default" hasIcon={false} icon={<div />} disabled/>
-          <PrimaryButton text="See My Projects" size="small" hasIcon={false} icon={<div />} />
-          <PrimaryButton text="See My Projects" size="small" hasIcon={true} icon={<Mail />} />
         </div>
       </main>
 
@@ -50,15 +49,3 @@ export default function Home() {
     </>
   );
 }
-
-/* CountryBG */
-
-// position: absolute;
-// width: 998.08px;
-// height: 1089px;
-// left: 1064.94px;
-// top: -9px;
-
-// background: linear-gradient(113.14deg, rgba(0, 0, 0, 0.8) 27.48%, rgba(0, 0, 0, 0.63) 34.72%, rgba(0, 0, 0, 0) 80.93%), url(japan.jpg);
-// filter: blur(2px);
-/* CountryBG */
--- src/widgets/button/ui/Button.tsx
+++ src/widgets/button/ui/Button.tsx
@@ -11,7 +11,7 @@ export type TButtonProps = {
 
 export default function PrimaryButton({text = "", size = "default", glow = true, hasIcon = false, icon, disabled}: TButtonProps) {
   return (
-    <button disabled={disabled} className="w-fit rounded-[0.75rem] text-[1.5rem] p-[0.0625rem] bg-gradient-to-r from-[#FFF] from-0% to-[#FF3E00] to-100% disabled:pointer-events-none hover:scale-[1.06] drop-shadow-[0_0_40px_rgba(255,62,0,1)] transition-all duration-500 disabled:opacity-50 disabled:drop-shadow-none">
+    <button disabled={disabled} className="w-fit rounded-[0.75rem] text-[1.5rem] p-[0.0625rem] bg-gradient-to-r from-[#FFF] from-0% to-[#FF3E00] to-100% drop-shadow-[0_0_40px_rgba(255,62,0,1)] transition-all duration-500 disabled:pointer-events-none disabled:opacity-50 disabled:drop-shadow-none">
       <div className={cn("relative px-[2rem] py-[1.25rem] gap-[10px] rounded-[0.75rem] bg-gradient-to-br from-[#FF5B00] from-50% to-[#FF7C34] to-50% text-white",
         {
           "text-base px-[1.25rem] py-[0.75rem]": size === "small",
