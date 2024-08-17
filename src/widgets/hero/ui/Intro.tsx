8ede14fc65dbf31b3d85fd759bcbb573cd2f928e
Samuel Short <samuel.short@gmail.com>
2024-08-17 23:03:37 +0900

    Replace ' with &apos; in Intro.tsx to fix build issues

--- src/widgets/hero/ui/Intro.tsx
+++ src/widgets/hero/ui/Intro.tsx
@@ -14,7 +14,7 @@ export default function Intro() {
       animate={{ opacity: 1, x: 0.5 }}
       transition={{ duration: 0.3 }}
     >
-      <h3>Hi, I'm <span className="text-primary">Sam</span></h3>
+      <h3>Hi, I&apos;m <span className="text-primary">Sam</span></h3>
       <h1>Software<br/><span className="text-primary">Engineer /&gt;</span></h1>
       <p className="max-w-[30.75rem]">Passionate engineer with over 5 years of professional development and consulting experience. Always looking to expand knowledge and develop exciting, cutting edge products.</p>
       <Link href="/projects">
